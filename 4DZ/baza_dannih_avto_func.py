6236688b313d0ecf67bec3096797722556215953
MichailArnaut <Mikearno@yandex.ru>
2015-10-25 23:55:13 +0300

    доделал 4 дз

--- 4DZ/baza_dannih_avto_func.py
+++ 4DZ/baza_dannih_avto_func.py
@@ -1,6 +1,10 @@
 # coding: utf-8
 import pickle, os
 
+
+###   1 Задание    ### - преобразование части программ в функции
+# п.с. при написании команды 'конец' завершится работа программы по 1 заданию, и начнется работа программы по 2 заданию и так далее...
+
 avtomobili = []
 
 komands = ['ввод','вывод','запись','имя','мощь','больше','меньше','промежуток','конец']
@@ -19,18 +23,44 @@ def vivod():
 "промежуток" - поиск авто по мощи в промежутке значений
 "конец" - завершает работу программы''')
 
-def vvod():
-    komand = input('\nВведите одну из команд {}: '.format(komands)) 
+def vvod():   # функция выбора команды
+    komand = input('\nВведите одну из команд {}: '.format(komands))  
+    return komand
+
+
+def proverka_vvoda(komand):  # проверка ввода команды
     while komand not in komands:
-        komand = input('Нет такой команды, введите еще раз :')  
+        komand = input('Нет такой команды, введите еще раз :')
     return komand
 
 
 def vibor(i):
     print('\nВы выбрали {} данных'.format(komands[i]))
 
+def zapis():    # функция записи списка автомобилей в файл базы данных 
+    f = open('c:/python/data_avto.txt', 'wb')
+    pickle.dump(avtomobili,f)
+    f.close
+    print('Запись произведена')
 
-def vvod_avto():
+def zapis2(x):    # функция перезаписи определенного объекта в файле
+    f = open('c:/python/data_avto.txt', 'wb')
+    pickle.dump(x,f)
+    f.close
+    print('Перезапись произведена')    
+
+
+def zagr():    # функция открытия файла базы данных на чтение
+    try:
+        f = open('c:/python/data_avto.txt', 'rb')
+        avtomobili = pickle.load(f)
+    except IOError:
+        avtomobili = []
+    return avtomobili
+
+    
+def vvod_avto():   # функция ввода авто и мощности в список автомобилей
+    vibor(0)
     ext = '' 
     while ext !='выход':
             
@@ -46,115 +76,197 @@ def vvod_avto():
         print(avtomobili)
         ext = input('''\nДля окончания ввода данных в базу, напишите "выход",
 Для продолжения нажмите "Enter":''')
+    return avtomobili
 
 
-def vivod_avto():
+def vivod_avto():   # функция вывода отсортированного списка автомобилей
+    vibor(1) 
     sort_avtomobili = sorted(avtomobili)
     for i in sort_avtomobili:
         print(i)
-
-
-def zagr():
-    f = open('c:/python/data_avto.txt', 'rb')
-    avtos = pickle.load(f)
-    return avtos
-
-
-def zapis():
-    f = open('c:/python/data_avto.txt', 'wb')
-    sort_avtomobili = sorted(avtomobili)
-    pickle.dump(sort_avtomobili,f)
-    f.close
-
-
-def name():
-    imya = input('Введите часть имени или полное имя: ')
-    for avto_mosh in avtos:
-        if imya in avto_mosh:
-            print(avto_mosh)
 
 
 def konec():
     print('больше нет авто')
 
 
-def mosh():
+def name():   # функция поиска авто по названию марки или части
+    avtos = zagr() 
+    imya = input('Введите часть имени или полное имя: ')
+    for avto_mosh in avtos:
+        if imya in avto_mosh:
+            print(avto_mosh)
+    konec()
+
+
+
+
+def mosh(): # функция поиска авто по мощности
+    avtos = zagr()
     mosh = int(input('Введите мощь авто: '))
     for avto_mosh in avtos:
         avmosh = avto_mosh.split()[1]
         if mosh == int(avmosh):
-            print(avto_mosh)    
+            print(avto_mosh)
+    konec()
 
 
-def bolshe():
+def bolshe():   # функция поиска авто по мощности больше заданной
+    avtos = zagr()
     mosh = int(input('введите наименьшую мощь: '))
     for avto_mosh in avtos:
         avmosh = avto_mosh.split()[1]
         if int(avmosh) >= mosh:
             print(avto_mosh)
+    konec()
 
 
-def menshe():
+def menshe():   # функция поиска авто по мощности меньше заданной
+    avtos = zagr()
     mosh = int(input('введите наибольшую мощь: '))
     for avto_mosh in avtos:
         avmosh = avto_mosh.split()[1]
         if int(avmosh) <= mosh:
             print(avto_mosh)
+    konec() 
 
 
-def promeg():
+def promeg():  # функция поиска авто по мощности в промежутке
+    avtos = zagr()
     mosh1 = int(input('введите наименьшую мощь в промежутке: '))
     mosh2 = int(input('введите наибольшую мощь в промежутке: '))
     for avto_mosh in avtos:
         avmosh = avto_mosh.split()[1]
         if mosh1 <= int(avmosh) <= mosh2:
             print(avto_mosh)
+    konec() 
 
 vivod()
 
 while komand != 'конец':
 
-    komand = vvod()
+    komand = proverka_vvoda(vvod())
 
     if komand == komands[0]:
-        vibor(0)
         vvod_avto()
 
     if komand == komands[1]:
-        vibor(1)
         vivod_avto()
 
     if komand == komands[2]:
-        vibor(2)
         zapis()
 
     if komand == komands[3]:
-        avtos = zagr()
         name()
-        konec()
-
+        
     if komand == komands[4]:
-        avtos == zagr()
         mosh()
-        konec()
 
     if komand == komands[5]:
-        avtos == zagr()
         bolshe()
-        konec()
 
     if komand == komands[6]:
-        avtos == zagr()
         menshe()
-        konec()
 
     if komand == komands[7]:
-        avtos == zagr()
         promeg()
-        konec()
+
 
     
-        
+###   2 Задание    ### - добавление команд и их функций в словарь
+
+def zavershit():
+    komand = 'конец'
+    return komand
+
+# komands = ['ввод','вывод','запись','имя','мощь','больше','меньше','промежуток','конец']
+
+FUNCS = {
+    'ввод':vvod_avto,
+    'вывод':vivod_avto,
+    'запись':zapis,
+    'имя':name,
+    'мощь':mosh,
+    'больше':bolshe,
+    'меньше':menshe,
+    'промежуток':promeg,
+    'конец':zavershit
+    }
+
+vivod()
+komand = ''
+while komand != 'конец':
+
+    komand = proverka_vvoda(vvod())
+    FUNCS[komand]()
+    
+
+###   3 Задание    ### - добавление операций редактирование по мощности и удаление авто из файла с базой данных
+
+def vivod():
+    print ('''
+"ввод" - ввод данных в список
+"вывод" - вывод данных из списка по алфавиту
+"запись" - запись списка в базу данных
+"имя" - поиск автомобиля по названию или части названия
+"мощь" - поиск по мощи
+"больше" - поиск по мощи, больше заданной
+"меньше" - поиск по мощи, меньше заданной
+"промежуток" - поиск авто по мощи в промежутке значений
+"удаление" - изменяет мощность авто в базе данных
+"редактирование" - удаляет авто из базы данных
+"конец" - завершает работу программы''')
+
+def ydal():
+    avtomobili = zagr()
+    print (avtomobili)
+    vibor(8)
+    imya = input('Введите полностью марку авто: ')
+    mosh = input('Введите мощность авто: ')
+    for avto_mosh in avtomobili:
+        avname,avmosh = avto_mosh.split()
+        if imya == avname and mosh == avmosh:
+            avtomobili.remove(avto_mosh)
+    zapis2(avtomobili)
+
+def redakt():
+    avtomobili = zagr()
+    print (avtomobili)
+    vibor(9)
+    imya = input('Введите полностью марку авто: ')
+    mosh = input('Введите мощность авто: ')
+    mosh2 =input('Введите новую мощность авто: ')
+    for avto_mosh in avtomobili:
+        avname,avmosh = avto_mosh.split()
+        if imya == avname and mosh == avmosh:
+            avtomobili.remove(avto_mosh)
+            avtomobili.append(avname + ' ' + mosh2)
+    zapis2(avtomobili)
+    
+komands = ['ввод','вывод','запись','имя','мощь','больше','меньше','промежуток','удаление','редактирование','конец']
+
+FUNCS = {
+    'ввод':vvod_avto,
+    'вывод':vivod_avto,
+    'запись':zapis,
+    'имя':name,
+    'мощь':mosh,
+    'больше':bolshe,
+    'меньше':menshe,
+    'промежуток':promeg,
+    'удаление':ydal,
+    'редактирование':redakt,
+    'конец':zavershit
+    }
+
+vivod()
+komand = ''
+while komand != 'конец':
+
+    komand = proverka_vvoda(vvod())
+    FUNCS[komand]()
+
+
 
 
 
